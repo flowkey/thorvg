c913ff131810461649bcf6a505749d8e233d67b0
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2026-05-13 13:02:12 +0200

    Change requird iOS versions from 26 to 16.4> - the minumum required version to compile

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ThorVGPackage",
-    platforms: [.iOS(.v26), .macOS(.v26)],
+    platforms: [.iOS("16.4"), .macOS(.v26)],
     products: [
         .library(name: "ThorVGCpp", targets: ["ThorVGCpp"]),
     ],
