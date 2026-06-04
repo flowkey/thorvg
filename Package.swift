c3d9b63886717f61ea92408579cdee0694ff3547
Kiran Boyle <86478874+AKiranB@users.noreply.github.com>
2026-06-04 10:25:03 +0530

    Change deployment target (#1)
    * Change requird iOS versions from 26 to 16.4> - the minumum required version to compile
    
    * Try to fix mobile-app build
    
    * Remove useless guards
    
    * Stuff
    
    * Fix
    
    * Fix

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ThorVGPackage",
-    platforms: [.iOS(.v26), .macOS(.v13)],
+    platforms: [.iOS("15.6"), .macOS(.v13)],
     products: [
         .library(name: "ThorVGCpp", targets: ["ThorVGCpp"]),
     ],
