480d5e911528dcc1981f11528cab8c608a3cee1b
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2026-06-04 06:43:24 +0200

    Fix

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ThorVGPackage",
-    platforms: [.iOS("15.6"), .macOS(.v13)],
+    platforms: [.iOS("v15.6"), .macOS(.v13)],
     products: [
         .library(name: "ThorVGCpp", targets: ["ThorVGCpp"]),
     ],
